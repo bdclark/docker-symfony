29fdc0f2666e91e6e97c7c4fd09ac2133d0fd4a5
Brian Clark <brian.clark@steadyserv.com>
2015-10-28 13:49:26 -0400

    fix fpm upstream

--- nginx-symfony/docker-entrypoint.sh
+++ nginx-symfony/docker-entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -e
 
+cat << EOF > /etc/nginx/conf.d/00-upstream.conf
+upstream php_fpm {
+    server $PHP_FPM_PORT_9000_TCP_ADDR:$PHP_FPM_PORT_9000_TCP_PORT;
+}
+EOF
+
 if [ "$NGINX_ENABLE_DEV" = true ]; then
     sed -i 's/<NGINX_ENV>/(app|app_dev|config)/' /etc/nginx/conf.d/default.conf
 else
     sed -i 's/<NGINX_ENV>/app/' /etc/nginx/conf.d/default.conf
 fi
 
-exec "$@"
+if [[ $# -eq 0 ]]; then
+    exec nginx -g 'daemon off;'
+else
+    exec "$@"
+fi
